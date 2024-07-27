3afe0765f2ad0352b0e9e22e9db54dd642aef690
GreggtheDev <greggthedev@gmail.com>
2024-07-26 21:08:24 -0400

    created constructor with name, age and grade parameters

--- src/main/java/school/management/Student.java
+++ src/main/java/school/management/Student.java
@@ -6,3 +6,11 @@ public class Student {
     private String name;
     private int age;
     private String grade;
+
+    /* Constructor to initialize a Student object.@param name the name of the student
+    @param age the age of the student @param grade the grade of the student */
+    public Student(String name, int age, String grade) {
+        this.name = name;
+        this.age = age;
+        this.grade = grade;
+    }
