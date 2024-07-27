8d8e333abb931904828a8129a1395b0c9171c3fe
GreggtheDev <greggthedev@gmail.com>
2024-07-26 21:50:37 -0400

    added constructor to initialize school object

--- src/main/java/school/management/School.java
+++ src/main/java/school/management/School.java
@@ -8,3 +8,10 @@ public class School {
     private String name;
     private List<Student> students;
     private List<Teacher> teachers;
+
+    // Constructor to initialize school object
+    public School(String name) {
+        this.name = name;
+        this.students = new ArrayList<>();
+        this.teachers = new ArrayList<>();
+    }
