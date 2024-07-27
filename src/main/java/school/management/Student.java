2d920c6b091b930b6f1a71cbf7ba179f544a0f2a
GreggtheDev <greggthedev@gmail.com>
2024-07-26 21:03:02 -0400

    created student class

---
+++ src/main/java/school/management/Student.java
@@ -0,0 +1,8 @@
+package school.management;
+
+
+// This class represents a Student with a name, age, and grade.
+public class Student {
+    private String name;
+    private int age;
+    private String grade;
