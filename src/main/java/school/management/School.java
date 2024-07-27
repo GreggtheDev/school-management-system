c49b6ce0131d3085abbcec191dbfaa2864a8ac46
GreggtheDev <greggthedev@gmail.com>
2024-07-26 21:48:40 -0400

    created school class

---
+++ src/main/java/school/management/School.java
@@ -0,0 +1,10 @@
+package school.management;
+
+import java.util.ArrayList;
+import java.util.List;
+
+// School class with name, student and teachers
+public class School {
+    private String name;
+    private List<Student> students;
+    private List<Teacher> teachers;
